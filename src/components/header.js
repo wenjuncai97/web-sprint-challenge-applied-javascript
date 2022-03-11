d901d557ce1d6bdd518e8b6836c26284849b44db
Nathan Cai <wenjuncai97@gmail.com>
2022-03-11 00:13:52 -0500

    finished step 1

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,3 @@
-const Header = (title, date, temp) => {
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -11,15 +10,34 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
-}
+const Header = (title, date, temp) => {
+  const header = document.createElement('div')
+  const headerDate = document.createElement('span')
+  const headerTitle = document.createElement('h1')
+  const headerTemp = document.createElement('span')
 
+  header.classList.add('header')
+  headerDate.classList.add('date')
+  headerTemp.classList.add('temp')
+
+  headerDate.textContent = date;
+  headerTitle.textContent = title;
+  headerTemp.textContent = temp;
+
+  header.appendChild(date)
+  header.appendChild(title)
+  header.appendChild(temp)
+
+  return header;
+}
+// TASK 2
+// ---------------------
+// Implement this function taking a css selector as its only argument.
+// It should create a header using the Header component above, passing arguments of your choosing.
+// It should append the header to the element in the DOM that matches the given selector.
+//
 const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
+
 }
 
 export { Header, headerAppender }
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,9 @@ module.exports = {
     }),
   ],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
     compress: true,
     port: 3000,
   },
